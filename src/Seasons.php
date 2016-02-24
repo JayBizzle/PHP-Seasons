208bc201c0475f57c9f18d516aee2bbf635d277f
Mark Beech <mbeech@mark-beech.co.uk>
2016-02-24 17:11:17 -0500

    Applied fixes from StyleCI

--- src/Seasons.php
+++ src/Seasons.php
@@ -73,6 +73,7 @@ class Seasons
     public function get($date = null)
     {
         $method = 'get'.ucfirst($this->methodology);
+
         return $this->$method($date);
     }
 
@@ -88,15 +89,14 @@ class Seasons
 
         $timestamps[] = strtotime($date);
         sort($timestamps);
-        
+
         $date = (is_null($date)) ? date('d/m/Y', time()) : $date;
-        
-        if(max($timestamps) == strtotime($date)) {
+
+        if (max($timestamps) == strtotime($date)) {
             return 'Winter - hard';
         } else {
             return $this->seasons[array_search(strtotime($date), $timestamps)];
         }
-        
     }
 
     public function astronomicalTimestamps($year)
@@ -142,30 +142,27 @@ class Seasons
         return $this;
     }
 
-
     public function astronomical()
     {
         $this->methodology = 'astronomical';
+
         return $this;
     }
 
-
     /**
-     *
      * @Get Timestamp for Vernal Equinox
      * beginning of spring in the Northern Hemisphere and autumn in the Southern Hemisphere.
      *
      * @param int $year
      *
      * @return int
-     *
      */
-    public function getSpringEquinox($year, $timezone='Etc/GMT')
+    public function getSpringEquinox($year, $timezone = 'Etc/GMT')
     {
         return $this->getEquinox(79.3125, $year);
     }
 
-    public function getAutumnEquinox($year, $timezone='Etc/GMT')
+    public function getAutumnEquinox($year, $timezone = 'Etc/GMT')
     {
         return $this->getEquinox(265.718, $year);
     }
@@ -177,35 +174,33 @@ class Seasons
         $gmt = gmmktime(0, 0, 0, 1, 1, 2000);
 
         $days_from_base = $days + ($year - 2000) * 365.2425;
-        $seconds_from_base = $days_from_base*86400;
+        $seconds_from_base = $days_from_base * 86400;
 
         $equinox = round($gmt + $seconds_from_base);
+
         return $equinox;
     }
-
-
-
 
     public function getSoltices($year)
     {
         date_default_timezone_set('UTC');
-        $date= $year.'/01/01';
+        $date = $year.'/01/01';
 
-        $end_date=$year.'/12/31';
+        $end_date = $year.'/12/31';
         $i = 0;
         //loop through the year
-        while(strtotime($date)<=strtotime($end_date)) { 
-            $sunrise=date_sunrise(strtotime($date),SUNFUNCS_RET_DOUBLE,31.47,35.13,90,3);
-            $sunset=date_sunset(strtotime($date),SUNFUNCS_RET_DOUBLE,31.47,35.13,90,3);
+        while (strtotime($date) <= strtotime($end_date)) {
+            $sunrise = date_sunrise(strtotime($date), SUNFUNCS_RET_DOUBLE, 31.47, 35.13, 90, 3);
+            $sunset = date_sunset(strtotime($date), SUNFUNCS_RET_DOUBLE, 31.47, 35.13, 90, 3);
             //calculate time difference
-            $delta = $sunset-$sunrise;
+            $delta = $sunset - $sunrise;
             //store the time difference
             $delta_array[$i] = $delta;
             //store the date
             $dates_array[$i] = strtotime($date);
             $i++;
             //next day
-            $date = date("Y-m-d",strtotime("+1 day",strtotime($date)));
+            $date = date('Y-m-d', strtotime('+1 day', strtotime($date)));
         }
 
         $dates['shortest'] = $dates_array[array_search(min($delta_array), $delta_array)];
@@ -213,5 +208,4 @@ class Seasons
 
         return $dates;
     }
-    
 }
